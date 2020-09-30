19fe09dad9914b1e32fdc1472cb0a2a5ca4281f6
khaiCM <minhkhai97dbp@gmail.com>
2020-09-30 23:38:40 +0700

    Commit add pipes validate

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { TaskStatus } from './../task.model';
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" an in valid status`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
